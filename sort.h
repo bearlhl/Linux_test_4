78afe085d4ed80d29d278b2a6a4792125cd5793a
bearlhl <894803413@qq.com>
2017-05-29 23:09:57 +0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,11 @@
+
+void BubbleSort(int a[],int n);      //冒泡排序
+void InsertSort(int a[],int n);      //插入排序
+void SelectSort(int a[],int n);      //选择排序
+void QuickSort(int a[],int left,int right);  //快速排序
+void shellInsert(int array[],int n,int dk);  //希尔排序
+int dkHibbard(int t,int k);
+void HeapAdjust(int array[],int i,int nLength);      //堆排序
+//堆排序算法
+void HeapSort(int array[],int length);
+
